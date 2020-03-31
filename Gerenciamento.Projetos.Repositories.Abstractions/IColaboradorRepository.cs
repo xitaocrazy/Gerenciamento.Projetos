9068b2c29db78cceebf79d3df1d27862b5671667
Guido <guido.warken@gmail.com>
2020-03-31 11:45:09 -0300

    feat(Gerenciamento.Projetos): Adiciona a abstração do repositorio.

---
+++ Gerenciamento.Projetos.Repositories.Abstractions/IColaboradorRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gerenciamento.Projetos.Entities;
+
+namespace Gerenciamento.Projetos.Repositories.Abstractions
+{
+    public interface IColaboradorRepository
+    {
+        Task AddColaboradorAsync(Colaborador colaborador);
+        Task UpdateColaboradorAsync(Colaborador colaborador);
+        Task RemoveColaboradorAsync(Colaborador colaborador);
+        Task<Colaborador> FindByIdAsync(Guid id);
+    }
+}
---
+++ Gerenciamento.Projetos.Repositories.Abstractions/IProjetoRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Gerenciamento.Projetos.Entities;
+
+namespace Gerenciamento.Projetos.Repositories.Abstractions
+{
+    public interface IProjetoRepository
+    {
+        Task AddProjetoAsync(Projeto projeto);
+        Task UpdateProjetoAsync(Projeto projeto);
+        Task RemoveProjetoAsync(Projeto projeto);
+        Task<Projeto> FindByIdAsync(Guid id);
+    }
+}
